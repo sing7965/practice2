39fa8314f74687fb0d20620cf59bcf7898c6d333
wwwsa <wwwsa@LAPTOP-86OV556F>
2019-03-09 13:28:36 -0500

    added getter and setter

---
+++ GITPractice/src/gitpractice/getSet.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitpractice;
+
+/**
+ *
+ * @author wwwsa
+ */
+public class getSet {
+    
+    private double heee;
+    private double beee;
+
+    /**
+     * @return the heee
+     */
+    public double getHeee() {
+        return heee;
+    }
+
+    /**
+     * @param heee the heee to set
+     */
+    public void setHeee(double heee) {
+        this.heee = heee;
+    }
+
+    /**
+     * @return the beee
+     */
+    public double getBeee() {
+        return beee;
+    }
+
+    /**
+     * @param beee the beee to set
+     */
+    public void setBeee(double beee) {
+        this.beee = beee;
+    }
+    
+}
